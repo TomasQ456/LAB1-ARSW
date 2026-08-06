b905a542c28b9ce4a920d7e9adb04bfca4c4831b
TomasQ456 <tomas.quiceno-o@mail.escuelaing.edu.co>
2026-08-05 22:11:18 -0500

    Added: point 2

--- src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -15,9 +15,10 @@ public class Main {
     
     public static void main(String a[]){
         HostBlackListsValidator hblv=new HostBlackListsValidator();
-        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55");
+        int numThreads=8;
+        List<Integer> blackListOcurrences=hblv.checkHost("202.24.34.55", numThreads);
         System.out.println("The host was found in the following blacklists:"+blackListOcurrences);
-        
+
     }
     
 }
